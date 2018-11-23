3adc286898e888bbe8b75fb7373c0c03870a8dff
henry <aguinagahenry@gmail.com>
2018-11-22 21:02:43 -0500

    First commit

--- CollectionView-1/ViewController.swift
+++ CollectionView-1/ViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    var colors : [UIColor] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        colors = [.black, .white, .red, .blue, .green]
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        var color = colors[indexPath.row]
+        cell.contentView.backgroundColor = color
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.view.backgroundColor = colors[indexPath.row]
+    }
+   
 
 
 }
